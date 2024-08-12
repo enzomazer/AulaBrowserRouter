bc3d85481909dd6d70c848f542ae83109faf2b09
enzomazer <enzokmitani@gmail.com>
2024-08-12 17:28:25 -0400

    dd
    dawd

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header"
 import { Link } from "react-router-dom";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "../global.css"
 
 export default function Home() {
     return(
@@ -9,6 +12,23 @@ export default function Home() {
             <Link to="/produtos">
             <h3>comprar</h3>
             </Link>
+            <Carousel
+            infiniteloop
+            useKeyboardArrows
+            autoPlay
+            showArrows={true}
+            showStatus={false}
+            showThumbs={false}
+            dynamicHeight
+            className="carrosas"
+            >
+                <div className="imagem1">
+                    <img className="imagem1" src="public\uatizapi.jpg"></img>
+                </div>
+                <div className="imagem2">
+                    <img className="imagem2" src="public\frogstomp.jpg"></img>
+                </div>
+            </Carousel>
         </div>
     )
 }
